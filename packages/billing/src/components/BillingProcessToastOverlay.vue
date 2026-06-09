f7ffe470e20e5033eed74e05924b3d4e6b7f2358
Jose Armando <jarmandocramirez@gmail.com>
2026-06-09 23:13:59 +0200

    Ajusta loader minimalista de emision

--- packages/billing/src/components/BillingProcessToastOverlay.vue
+++ packages/billing/src/components/BillingProcessToastOverlay.vue
@@ -60,7 +60,11 @@ const titleClass = computed(() => {
             <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" />
           </svg>
 
-          <span v-else class="h-6 w-6 animate-spin rounded-full border-2 border-white/40 border-t-white"></span>
+          <span v-else class="flex gap-1.5" aria-hidden="true">
+            <span class="size-2 animate-bounce rounded-full bg-white"></span>
+            <span class="size-2 animate-bounce rounded-full bg-white [animation-delay:0.2s]"></span>
+            <span class="size-2 animate-bounce rounded-full bg-white [animation-delay:0.4s]"></span>
+          </span>
         </div>
 
         <div class="min-w-0 flex-1 px-4 py-3">
